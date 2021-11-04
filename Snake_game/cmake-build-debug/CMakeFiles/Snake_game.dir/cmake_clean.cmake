8bbca1844e6159a54905de67a86011da5b0c61d4
Mitró Gyuri <57859881+deckard04@users.noreply.github.com>
2021-11-04 15:14:54 +0100

    Snake game v1.0

---
+++ Snake_game/cmake-build-debug/CMakeFiles/Snake_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake_game.dir/main.cpp.o"
+  "Snake_game"
+  "Snake_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
